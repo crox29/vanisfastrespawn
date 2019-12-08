133e2a3770819ac811855bae714cecebe030ccb7
WolfyDeRetour <49119628+WolfyDeRetour@users.noreply.github.com>
2019-12-08 01:21:09 +0100

    Add files via upload

---
+++ VanisFastRespawn.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Vanis FastRespawn
+// @match        *.vanis.io/*
+// @author       Wolfy_
+// @grant        none
+// @require      https://github.com/WolfyDeRetour/vanisfastrespawn/raw/master/VanisFastRespawn.core.js
+// @run-at       document-ready
+// ==/UserScript==
